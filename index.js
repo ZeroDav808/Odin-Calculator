6e1beef0947af4cdddd7d03ff7d98a829d9d38ca
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-10-23 14:03:59 -0500

    Created an operate function that takes in numbers and operations and calculates values

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ function divide(a,b){
 
     return a / b;
 }
+
+const values = {
+    num1: null,
+    operation: null,
+    num2: null,
+}
+
+function operate(num1, num2, operation){
+    switch(operation){
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1,num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Invalid Operation! Could not complete computation!';
+    }
+}
