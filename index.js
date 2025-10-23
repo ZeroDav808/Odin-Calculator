7786563005e925fb8e33fb47b7b71b86c7f3ede0
ZeroDav808 <199318346+ZeroDav808@users.noreply.github.com>
2025-10-23 15:53:50 -0500

    Added the ability to add two numbers and enter equal to get the result

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ const values = {
     num1: null,
     operation: null,
     num2: null,
+    result: null,
 }
 
 function operate(num1, num2, operation){
@@ -39,9 +40,74 @@ function operate(num1, num2, operation){
     }
 }
 
+function clear(){
+    console.log('Clear function has been called.');
+    display.textContent = '';
+
+    for(let key in values){
+        values[key] = null;
+        console.log(`${key} has been reset to ${values[key]}`);
+    }
+}
+
+function handleNumber(value){
+    console.log('Number function has been called');
+
+    if(!values.operation){
+        if(!values.num1){
+            values.num1 = value;
+        } else {
+            values.num1 += value;
+        }
+        display.textContent = values.num1;
+    } else {
+        if(!values.num2){
+            values.num2 = value;
+        } else {
+            values.num2 += value;
+        }
+        display.textContent = values.num2;
+    }
+}
+
+function handleOperation(value){
+    values.operation = value;
+    display.textContent = '';
+}
+
+function handleEqual(){
+    values.result = operate(+values.num1, +values.num2, values.operation);
+    display.textContent = values.result;
+}
+
+function handleClick(event){
+    const classClicked = event.target.className;
+    const value = event.target.textContent;
+
+    console.log(`Class clicked is ${classClicked} and the value is ${value}`);
+
+    switch(classClicked){
+        case 'number':
+            handleNumber(value);
+            break;
+        case 'operator':
+            handleOperation(value);
+            break;
+        case 'clear':
+            clear();
+            break;
+        case 'equal':
+            handleEqual();
+            break;
+        default:
+            console.log('Invalid item?');
+            break;
+    }
+}
+
 const btnContainer = document.querySelector('.calculator-body');
 const display = document.querySelector('.calculator-display');
 
-btnContainer.addEventListener('click', (e)=>{
-    display.textContent = e.target.textContent;
-})
+btnContainer.addEventListener('click', function(e){
+    handleClick(e)
+});
